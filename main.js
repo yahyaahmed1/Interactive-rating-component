ec4ef3c92aef867cdf5fb1c70b7e6fcc15b90c2f
Yahya-Ahmed <yahyaahmedfarouk6@gmail.com>
2022-10-30 12:09:43 +0200

    all done

---
+++ main.js
@@ -0,0 +1,23 @@
+let ratingCard = document.querySelector(".rating");
+let thankCard = document.querySelector(".thanking");
+let rate = document.querySelectorAll("li");
+let btn = document.querySelector("button");
+let resMsg = document.querySelector(".result-rating");
+
+for (let i = 0; i < rate.length; i++) {
+  rate[i].onclick = function () {
+    rate.forEach(num => {
+      if (num.classList.contains("checked")) {
+        num.className = "";
+      }
+    });
+    rate[i].classList.toggle("checked");
+    btn.onclick = function () {
+      console.log(ratingCard);
+      ratingCard.style.display = "none";
+      thankCard.style.display = "grid";
+      let msg = document.createTextNode(`You selected ${rate[i].innerHTML} out of 5`);
+      resMsg.appendChild(msg);
+    };
+  };
+};
